e7654cce87c52fe1dbbc28d374a37c8e62c73d64
Lotte van der Klei <115008562+lottevanderklei2@users.noreply.github.com>
2023-02-09 09:53:30 +0100

    try

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ from io import StringIO
 
 import streamlit as st
 
-bingo = st.file_uploader("upload file", type={"csv", "txt"})
-if bingo is not None:
-    df = pd.read_csv(bingo)
-st.write(df)
+# bingo = st.file_uploader("upload file", type={"csv", "txt"})
+# if bingo is not None:
+#     df = pd.read_csv(bingo)
+# st.write(df)
 
 # uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
 # for uploaded_file in uploaded_files:
@@ -15,25 +15,25 @@ st.write(df)
 #     st.write("filename:", uploaded_file.name)
 #     st.write(bytes_data)
 
-# uploaded_file = st.file_uploader("Choose a file")
-# if uploaded_file is not None:
-#     # To read file as bytes:
-#     bytes_data = uploaded_file.getvalue()
-#     st.write(bytes_data)
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
 
-#     # To convert to a string based IO:
-#     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-#     st.write(stringio)
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write(stringio)
 
-#     # To read file as string:
-#     string_data = stringio.read()
-#     st.write(string_data)
+    # To read file as string:
+    string_data = stringio.read()
+    st.write(string_data)
 
-#     # Can be used wherever a "file-like" object is accepted:
-#     dataframe = pd.read_csv(uploaded_file)
-#     st.write(dataframe)
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
 
-playlist = df
+playlist = dataframe
 aantal_kaarten = 2
 seed_num = 2407
 
