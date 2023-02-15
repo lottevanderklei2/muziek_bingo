ee4d985f452412fa1f59bcb72658c97e74d2911a
Lotte van der Klei <115008562+lottevanderklei2@users.noreply.github.com>
2023-02-15 16:23:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,16 +5,9 @@ from io import StringIO
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
-    # To read file as bytes:
     bytes_data = uploaded_file.getvalue()
-
-    # To convert to a string based IO:
     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-
-    # To read file as string:
     string_data = stringio.read()
-
-    # Can be used wherever a "file-like" object is accepted:
     dataframe = pd.read_csv(uploaded_file)
     st.write(dataframe)
 else: 
@@ -28,7 +21,6 @@ seed_num = random_seed
 
 card_num = st.number_input('Insert number of cards', step = 1 )
 st.write('The current number is ', card_num)
-# aantal_kaarten = card_num
   
 if card_num is not None:
     aantal_kaarten = card_num
@@ -42,7 +34,6 @@ def kaart_generator(playlist, seed_num):
     random.shuffle(nums)
     playlist['random'] = nums
     new_df = playlist.sort_values("random")
-#     test = pd.DataFrame([[' B '],[ ' I '], [' N '], [' G '], [' O ']], columns=['B', 'I', 'N', 'G', 'O'])
     data = list(zip(new_df['title_and_artist'][0:5], 
     new_df['title_and_artist'][5:10],
     new_df['title_and_artist'][10:15],
@@ -52,10 +43,6 @@ def kaart_generator(playlist, seed_num):
     test2 = pd.DataFrame([' B ', ' I ', ' N ', ' G ', ' O ']).T
     df2 = test2.append(data, ignore_index=True)
     df2 = df2.append(test, ignore_index=True)
-    
-#     df2 = pd.DataFrame(data) #, columns=['B', 'I', 'N', 'G', 'O'])
-#     df2 = df2.append(test, ignore_index=True)
-#     df2 = df2.append(test2, ignore_index=True)
     df2[2][2] = "BINGO"
     return df2
 
@@ -65,8 +52,21 @@ def bingo_kaarten_generator(playlist, aantal_kaarten, seed_num):
         kaarten_list.append(kaart_generator(playlist, seed_num + i))
     return kaarten_list
 
+hide_table_row_index = """
+            <style>
+            thead tr th:first-child {display:none}
+            tbody th {display:none}
+            </style>
+            """
+
+# Inject CSS with Markdown
+st.markdown(hide_table_row_index, unsafe_allow_html=True)
+
+# # Display a static table
+# st.table(df)
+
 for i in range(0, aantal_kaarten):
-    st.dataframe(bingo_kaarten_generator(playlist, aantal_kaarten, seed_num)[i])
+    st.table(bingo_kaarten_generator(playlist, aantal_kaarten, seed_num)[i])
     
 test = pd.concat(bingo_kaarten_generator(playlist, aantal_kaarten, seed_num))
     
