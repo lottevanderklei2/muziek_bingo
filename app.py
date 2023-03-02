783154bc35d27eaaa6257feb0a6b71ac6ec6801f
Lotte van der Klei <115008562+lottevanderklei2@users.noreply.github.com>
2023-03-02 23:17:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ from io import StringIO
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
-    bytes_data = uploaded_file.getvalue()
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    string_data = stringio.read()
+#     bytes_data = uploaded_file.getvalue()
+#     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+#     string_data = stringio.read()
     dataframe = pd.read_csv(uploaded_file, sep = ';')
     st.write(dataframe)
 else: 
