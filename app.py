093ae5f8074cc4f9cef930fe0dfad4fa455d6f12
Lotte van der Klei <115008562+lottevanderklei2@users.noreply.github.com>
2023-05-24 22:16:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import random
 from openpyxl import reader,load_workbook,Workbook
-import weasyprint
+from pyppeteer import launch
 
 uploaded_file = st.file_uploader("Upload je afspeellijst in excel formaat")
 
@@ -64,17 +64,23 @@ def kaart_generator2(playlist, seed_num):
 st.table(kaart_generator2(playlist, seed_num))
 
 df = kaart_generator2(playlist, seed_num)
+# Display the pretty DataFrame in Streamlit
 st.dataframe(df.style.set_properties(**{'text-align': 'center'}))
 
 # Create a function to download DataFrame as PDF
-def download_as_pdf(df):
-    pdf = weasyprint.HTML(string=df.to_html()).write_pdf()
+async def download_as_pdf(df):
+    browser = await launch()
+    page = await browser.newPage()
+    html = df.to_html()
+    await page.setContent(html)
+    pdf = await page.pdf()
+    await browser.close()
     return pdf
 
 # Create a download button
 if st.button('Download as PDF'):
     with st.spinner('Generating PDF...'):
-        pdf_file = download_as_pdf(df)
+        pdf_file = await download_as_pdf(df)
         st.success('Download Completed!')
         st.download_button(
             label='Click to Download',
