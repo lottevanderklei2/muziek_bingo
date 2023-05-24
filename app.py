0863367eab27e0c94793a7aef286e8c6c195e830
Lotte van der Klei <115008562+lottevanderklei2@users.noreply.github.com>
2023-05-24 22:05:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -36,12 +36,12 @@ def kaart_generator2(playlist, seed_num):
     cols = ['B', 'I','N','G','O']
 #     test = pd.DataFrame(['  ', '  ', '  ', '  ', '  ']).T
 #     test2 = pd.DataFrame(['  ', '  ', '  ', '  ', '  ']).T
-#     df2 = pd.DataFrame(data) #, ignore_index=True)
+    df2 = pd.DataFrame(data) #, ignore_index=True)
 #     df2 = df2.concat(test, ignore_index=True)
 #     df2 = df2.concat(test2, ignore_index=True)
-    new_df.columns = cols
-    new_df['N'][2] = "BINGO"
-    return new_df
+    df2.columns = cols
+    df2['N'][2] = "BINGO"
+    return df2
 
 # df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
 
