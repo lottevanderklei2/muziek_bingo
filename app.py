80554da2dceab8c54cae277e089267c5d80f680f
Lotte van der Klei <115008562+lottevanderklei2@users.noreply.github.com>
2023-05-24 21:54:53 +0200

    tests2

--- app.py
+++ app.py
@@ -2,12 +2,8 @@ import streamlit as st
 import pandas as pd
 import random
 from openpyxl import reader,load_workbook,Workbook
-# from io import StringIO
 
-import pandas as pd
-import streamlit as st
-
-uploaded_file = st.file_uploader("Choose a file")
+uploaded_file = st.file_uploader("Upload je afspeellijst in excel formaat")
 
 if uploaded_file is not None:
     try:
@@ -16,78 +12,21 @@ if uploaded_file is not None:
     except Exception as e:
         st.error(f"Error: {str(e)}")
 else:
-    st.text('No file uploaded')
-
-# uploaded_file = st.file_uploader("Choose a file")
-# if uploaded_file is not None:
-#     dataframe = pd.read_csv(uploaded_file, sep = ';')
-#     st.write(dataframe)
-# else: 
-#     st.text('No file uploaded')
+    st.text('Je hebt nog geen bestand geüpload')
 
 playlist = pd.DataFrame(dataframe) 
 
-random_seed = st.number_input('Insert your favorite number', step=1)
-st.write('Your favorite number is ', random_seed)
+random_seed = st.number_input('Vul hier je favoriete nummer in', step=1)
+st.write('Jouw favoriete nummer is: ', random_seed)
 seed_num = random_seed
 
-card_num = st.number_input('Insert number of cards you want', step = 1 )
-st.write('The number of cars is ', card_num)
+# card_num = st.number_input('Vul hier in hoeveel bingokaartenje wil genereren', step = 1 )
+# st.write('Aantal bingokaarten ', card_num)
   
-if card_num is not None:
-    aantal_kaarten = card_num
-else: 
-    aantal_kaarten = 4
-
-def kaart_generator(playlist, seed_num):
-    random.seed(seed_num)
-    nums = list(range(1, 51)) 
-    random.shuffle(nums)
-    playlist['random'] = nums
-    new_df = playlist.sort_values("random")
-    data = list(zip(new_df['title_and_artist'][0:5], 
-    new_df['title_and_artist'][5:10],
-    new_df['title_and_artist'][10:15],
-    new_df['title_and_artist'][15:20],
-    new_df['title_and_artist'][20:25]))
-    cols = ['B', 'I','N','G','O']
-    df2 = pd.DataFrame(data) #, ignore_index=True)
-    df2.columns = cols
-    df2['N'][2] = "BINGO"
-    return df2
-
-def bingo_kaarten_generator(playlist, aantal_kaarten, seed_num):
-    kaarten_list = []
-    for i in range(0, aantal_kaarten):
-        kaarten_list.append(kaart_generator(playlist, seed_num + i))
-    return kaarten_list
-
-
-def highlight_bingo(ser):
-    highlight = 'background-color:#4A707D'
-    default = ''
-    return [highlight if 'BINGO' in str(e) else default for e in ser] 
-
-df_styled = []
-for i in range(0, aantal_kaarten):
-    df_styled.append(bingo_kaarten_generator(playlist, aantal_kaarten, seed_num)[i]
-     .style     
-     .hide_index()
-#      .set_caption("BINGO AURAI")
-     .set_properties(**{'background-color': 'white',                                                   
-                                    'color': 'black',                       
-                                    'border-color': 'white',
-                                    'text-align': 'center',
-                                    'text': 'bold', 
-                                    'font-family': 'Arial', 
-                                    'font-weight':'bold'})
-#      .apply(highlight_bingo, axis=0, subset=cols)
-     .set_table_styles([dict(selector='th', props=[('text-align', 'center')])]))
-
-for i in range(0, aantal_kaarten):
-    st.dataframe(df_styled[i])
-
-
+# if card_num is not None:
+#     aantal_kaarten = card_num
+# else: 
+#     aantal_kaarten = 4
 
 def kaart_generator2(playlist, seed_num):
     random.seed(seed_num)
@@ -114,25 +53,104 @@ def bingo_kaarten_generator2(playlist, aantal_kaarten, seed_num):
     kaarten_list = []
     for i in range(0, aantal_kaarten):
         kaarten_list.append(kaart_generator2(playlist, seed_num + i))
-    return kaarten_list    
+    return kaarten_list       
+    
+st.write(kaart_generator2(playlist, seed_num))
+    
+# def kaart_generator(playlist, seed_num):
+#     random.seed(seed_num)
+#     nums = list(range(1, 51)) 
+#     random.shuffle(nums)
+#     playlist['random'] = nums
+#     new_df = playlist.sort_values("random")
+#     data = list(zip(new_df['title_and_artist'][0:5], 
+#     new_df['title_and_artist'][5:10],
+#     new_df['title_and_artist'][10:15],
+#     new_df['title_and_artist'][15:20],
+#     new_df['title_and_artist'][20:25]))
+#     cols = ['B', 'I','N','G','O']
+#     df2 = pd.DataFrame(data) #, ignore_index=True)
+#     df2.columns = cols
+#     df2['N'][2] = "BINGO"
+#     return df2
+
+# def bingo_kaarten_generator(playlist, aantal_kaarten, seed_num):
+#     kaarten_list = []
+#     for i in range(0, aantal_kaarten):
+#         kaarten_list.append(kaart_generator(playlist, seed_num + i))
+#     return kaarten_list
+
+
+# def highlight_bingo(ser):
+#     highlight = 'background-color:#4A707D'
+#     default = ''
+#     return [highlight if 'BINGO' in str(e) else default for e in ser] 
+
+# df_styled = []
+# for i in range(0, aantal_kaarten):
+#     df_styled.append(bingo_kaarten_generator(playlist, aantal_kaarten, seed_num)[i]
+#      .style     
+#      .hide_index()
+# #      .set_caption("BINGO AURAI")
+#      .set_properties(**{'background-color': 'white',                                                   
+#                                     'color': 'black',                       
+#                                     'border-color': 'white',
+#                                     'text-align': 'center',
+#                                     'text': 'bold', 
+#                                     'font-family': 'Arial', 
+#                                     'font-weight':'bold'})
+# #      .apply(highlight_bingo, axis=0, subset=cols)
+#      .set_table_styles([dict(selector='th', props=[('text-align', 'center')])]))
+
+# for i in range(0, aantal_kaarten):
+#     st.dataframe(df_styled[i])
+
+
+
+# def kaart_generator2(playlist, seed_num):
+#     random.seed(seed_num)
+#     nums = list(range(1, 51)) 
+#     random.shuffle(nums)
+#     playlist['random'] = nums
+#     new_df = playlist.sort_values("random")
+#     data = list(zip(new_df['title_and_artist'][0:5], 
+#     new_df['title_and_artist'][5:10],
+#     new_df['title_and_artist'][10:15],
+#     new_df['title_and_artist'][15:20],
+#     new_df['title_and_artist'][20:25]))
+#     cols = ['B', 'I','N','G','O']
+#     test = pd.DataFrame(['  ', '  ', '  ', '  ', '  ']).T
+#     test2 = pd.DataFrame(['  ', '  ', '  ', '  ', '  ']).T
+#     df2 = pd.DataFrame(data) #, ignore_index=True)
+#     df2 = df2.append(test, ignore_index=True)
+#     df2 = df2.append(test2, ignore_index=True)
+#     df2.columns = cols
+#     df2['N'][2] = "BINGO"
+#     return df2
+    
+# def bingo_kaarten_generator2(playlist, aantal_kaarten, seed_num):
+#     kaarten_list = []
+#     for i in range(0, aantal_kaarten):
+#         kaarten_list.append(kaart_generator2(playlist, seed_num + i))
+#     return kaarten_list    
 
 
     
-test = pd.concat(bingo_kaarten_generator2(playlist, aantal_kaarten, seed_num))
+# test = pd.concat(bingo_kaarten_generator2(playlist, aantal_kaarten, seed_num))
     
-@st.cache
-def convert_df(df):
-    # IMPORTANT: Cache the conversion to prevent computation on every rerun
-    return df.to_csv(index=False, header=False, sep=';').encode('UTF-16')
+# @st.cache
+# def convert_df(df):
+#     # IMPORTANT: Cache the conversion to prevent computation on every rerun
+#     return df.to_csv(index=False, header=False, sep=';').encode('UTF-16')
 
-csv = convert_df(test)
+# csv = convert_df(test)
 
-st.download_button(
-    label="Download data as CSV",
-    data=csv,
-    file_name='bingo_kaarten.csv',
-    mime='text/csv',
-)
+# st.download_button(
+#     label="Download data as CSV",
+#     data=csv,
+#     file_name='bingo_kaarten.csv',
+#     mime='text/csv',
+# )
 
 
 
